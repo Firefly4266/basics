ef490029c5ab7b03f0916dcb57c3176be7908c14
Firefly4266 <johnathon@Icon-Base.(none)>
2016-12-05 20:06:55 -0800

    completed exercize

---
+++ story-app/app.js
@@ -0,0 +1,6 @@
+'use strict';
+var noun = prompt('please enter a noun.');
+var verb = prompt('please enter a verb');
+var adjective = prompt('Please enter an adjective.');
+var story = '<h2>There was a ' + noun + ' who ' + verb + ' very ' + adjective + ' until the cows came home.</h2>';
+document.write(story);
